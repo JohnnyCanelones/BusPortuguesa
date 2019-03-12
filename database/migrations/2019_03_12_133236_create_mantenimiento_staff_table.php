2d9fe68ba7317528360b1503cbc9991fc0983eac
JohnnyCanelones <johnnyjose13@gmail.com>
2019-03-12 13:35:54 -0400

    creando table pivote entre mantenimiento y personal

---
+++ database/migrations/2019_03_12_133236_create_mantenimiento_staff_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMantenimientoStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mantenimiento_staff', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('mantenimiento_id')->unsigned();
+            $table->foreign('mantenimiento_id')->references('id')->on('mantenimiento');
+            $table->integer('mecanico_id')->unsigned();
+            $table->foreign('mecanico_id')->references('id')->on('staff')->onDelete('cascade');
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mantenimiento_staff');
+    }
+}
