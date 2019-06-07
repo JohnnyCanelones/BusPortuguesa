2149e38764d80858f0a041e48698c1d3b655f0ba
JohnnyCanelones <johnnyjose13@gmail.com>
2019-06-07 15:15:31 -0400

    agregando cambios para heroku

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -18,16 +18,16 @@ class Admin
         if (! $request->user()->hasRole($user1, $user2)) {
             if (! $request->user()->hasRole('1', '')) {
                 if ($request->user()->hasRole('', '1')) {
-                    return '/mantenimiento';
+                    return redirect('/mantenimiento');
                 }
                 elseif ($request->user()->hasRole2('1', '')) {
-                    return '/personal';
+                    return redirect('/personal');
                 }
                 elseif ($request->user()->hasRole2('', '1')) {
-                    return '/almacen';
+                    return redirect('/almacen');
                 }
                 elseif ($request->user()->hasRole3('1')) {
-                    return '/operaciones';
+                    return redirect('/operaciones');
                 }
             }else {
                 
