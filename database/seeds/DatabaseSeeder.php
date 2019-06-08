bc8b533bf2f5ece9c8e252f82aa50e92eab74bc0
JohnnyCanelones <johnnyjose13@gmail.com>
2019-06-08 10:39:23 -0400

    arreglando mas fallos

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
 			UsersTableSeeder::class,
 			// StaffMonitoringTableSeeder::class,
-            // AlmacenSeeder::class,
-            // BusesSeeder::class,
+            AlmacenSeeder::class,
+            BusesSeeder::class,
             // WarehouseMonitoringTableSeeder::class,
             OccupationTableSeeder::class,
 
